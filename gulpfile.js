230bd10707967f77294e3208f2a4f3707b1e5835
i-break-codes <vaibhav@browserstack.com>
2016-11-20 13:08:21 +0530

    Initial commit, app almost completed

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS     = require('gulp-clean-css'),
+    uglify       = require('gulp-uglify'),
+    renameFiles  = require('gulp-rename');
+    
+  gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+      server: {
+        // baseDir: "app",
+        proxy: 'http://localhost:8888/scrum/',
+        // files: ['app/views/**/*.*', 'public/**/*.css', 'public/**/*.js'],
+        browser: 'google chrome',
+        port: 3005,
+        // open: false
+      }
+    });
+  });
+  
+  gulp.task('bs-reload', function () {
+    browserSync.reload();
+  });
+    
+  gulp.task('css', function () {
+    return gulp.src('src/stylesheets/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer('last 3 version'))
+      .pipe(gulp.dest('app/assets/stylesheets'))
+      .pipe(cleanCSS({debug: true}, function(details) {
+        console.log('Original Size : ' + details.name + ': ' + details.stats.originalSize + ' bytes');
+        console.log('Minified Size : ' + details.name + ': ' + details.stats.minifiedSize + ' bytes');
+      }))
+      .pipe(renameFiles({ suffix: '.min' }))
+      .pipe(gulp.dest('app/assets/stylesheets'))
+      .pipe(browserSync.reload({
+        stream:true
+      }));
+  });
+  
+  
+  gulp.task('js',function(){
+    return gulp.src('src/scripts/**/*.js')
+      .pipe(gulp.dest('app/assets/scripts'))
+      .pipe(uglify())
+      .pipe(renameFiles({ suffix: '.min' }))
+      .pipe(gulp.dest('app/assets/scripts'))
+      .pipe(browserSync.reload({
+        stream: true, 
+        once: true
+      }));
+  });
+  
+  gulp.task('default', ['js', 'css', 'browser-sync'], function () {
+    gulp.watch("src/stylesheets/**/*.scss", ['css']);
+    gulp.watch("src/scripts/**/*.js", ['js']);
+    gulp.watch("app/*.html", ['bs-reload']);
+  });
---
+++ src/scripts/app.js
@@ -0,0 +1,202 @@
+var App = function() {
+  function init() {
+    preset();
+    draggable();
+    droppable();
+    openCard();
+    createTask();
+    closeModal();
+    printNotes();
+    editTask();
+    exitEditMode();
+  }
+  
+  function preset() {
+    $('#remove').on('click', function(e) {
+      e.preventDefault();
+    });
+    
+    var defaultTask = {
+      id: 1,
+      title: 'This is a sample task',
+      description: 'Sample tasks are useful to get started',
+      remote_url: 'https://asana.com/12345/1234',
+      assigned_to: 'Jon Doe',
+      status: 'pending'
+    }
+    
+    if(!LocalStorage.get('appInitialized', true)) {
+      LocalStorage.set('taskCounter', 1);
+      LocalStorage.set('task-1', JSON.stringify(defaultTask));
+      LocalStorage.set('appInitialized', true);
+    }
+  }
+  
+  function createTask() {
+    var source = $("#task-card-template").html();
+    var template = Handlebars.compile(source);
+    
+    $('#add-task').on('click', function(e) {
+      e.preventDefault();
+      $('#add-task-modal').removeClass('hide');
+      
+      $('#add-task-modal').find('form').on('submit', function(e) {
+        e.preventDefault();
+        var obj = {};
+        var params = $(this).serialize();
+        var splitParams = params.split('&');
+        
+        for(var i = 0, l = splitParams.length; i < l; i++) {
+          var keyVal = splitParams[i].split('=');
+          obj[keyVal[0]] = unescape(keyVal[1]);
+        }
+        
+        var iid = LocalStorage.get('taskCounter');
+        obj.id = ++iid;
+        obj.status = 'pending';
+        LocalStorage.set('task-' + obj.id, JSON.stringify(obj));
+        LocalStorage.set('taskCounter', iid);
+        
+        var newCard = template([obj]);
+        $('#dashboard #' + obj.status).append(newCard);
+        draggable();
+        
+        $('.close-modal').trigger('click');
+      });
+      
+    });
+  }
+  
+  function editTask() {
+    $('.card-details p').on('dblclick', function(e) {
+      e.stopPropagation();
+      $(this).attr('contenteditable','true').parents('.card').addClass('edit-mode');
+    });
+    
+    $('.card').find('p').on('input', function() {
+      var taskId = $(this).parents('.card').data('task-id');
+      var fieldToEdit = $(this).data('field');
+      var getTaskData = JSON.parse(LocalStorage.get('task-' + taskId));
+      
+      getTaskData[fieldToEdit] = $(this).text();
+      
+      LocalStorage.set('task-' + taskId, JSON.stringify(getTaskData));
+    });
+  }
+  
+  function exitEditMode() {
+    $(document).on('dblclick', function(e) {
+      $('.card').removeClass('edit-mode').find('[contenteditable]').removeAttr('contenteditable');
+    });
+  }
+  
+  function closeModal() {
+    $('.close-modal').on('click', function() {
+      $('.modal').addClass('hide');
+    })
+  }
+  
+  function draggable() {
+    $('.card').draggable({
+      handle: 'h5',
+      revert: true,
+      cursor: 'move',
+      start: function(event, ui) {
+        
+      },
+      stop: function(event, ui) {
+        
+      }
+    });
+  }
+  
+  function droppable() {
+    var droppableConfig = {
+      tolerance: 'pointer',
+      drop: function(event, ui) {
+        var elm = ui.draggable,
+            parentId = elm.parent().attr('id'),
+            currentId = $(this).attr('id'),
+            taskId = elm.data('task-id');
+        
+        if($(this).attr('id') == 'remove') {
+          //Deletes task
+          elm.remove();
+          LocalStorage.remove('task-' + taskId);
+          $('#removed-task-notification').text('Task removed successfully').removeClass('hide');
+          
+          
+          setTimeout(function() {
+            $('#removed-task-notification').text('').addClass('hide');
+          }, 3000);
+        } else {
+          //Moves task
+          if(parentId != currentId) {
+            $(elm).detach().removeAttr('style').appendTo(this);
+            
+            var getTaskData = JSON.parse(LocalStorage.get('task-' + taskId));
+            getTaskData.status = currentId;
+            
+            LocalStorage.set('task-' + taskId, JSON.stringify(getTaskData));
+          }
+        }
+        
+        $(this).removeClass('dragged-over');
+      },
+      over: function(event, ui) {
+        $(this).addClass('dragged-over');
+      },
+      out: function(event, ui) {
+        $(this).removeClass('dragged-over');
+      }
+    }
+    
+    $('#dashboard > div, #remove').droppable(droppableConfig);
+  }
+  
+  function openCard() {
+    $(document).on('click', '.expand-card', function(e) {
+      $(this).parent().toggleClass('expanded');
+      e.preventDefault();
+    });
+  }
+  
+  function getAllNotes() {
+    var getAllData = localStorage;
+    var getTasks = [];
+    
+    for(var data in getAllData) {
+      if(data.split('-')[0] == 'task') {
+        getTasks.push(JSON.parse(localStorage[data]));
+      }
+    }
+    
+    return getTasks;
+  }
+  
+  function printNotes() {
+    var source = $("#task-card-template").html();
+    var template = Handlebars.compile(source);
+    
+    var status = ['rejected', 'pending', 'development', 'testing', 'production'];
+    
+    for(var i = 0, l = status.length; i < l; i++) {
+      var result = App.getAllNotes().filter(function(obj) {
+        return obj.status == status[i];
+      });
+      
+      if(result) {
+        var cards = template(result);
+        $('#dashboard #' + status[i]).append(cards);
+        draggable();
+      }
+    }
+  }
+  
+  return {
+    init: init,
+    getAllNotes: getAllNotes
+  }
+}();
+
+App.init();
