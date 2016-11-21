c6aabb2d2cde4fcdfbfb3091232867b3bfaf3a6a
i-break-codes <vaibhav@browserstack.com>
2016-11-21 22:41:31 +0530

    compiled assets

--- app/assets/scripts/localstorage.js
+++ app/assets/scripts/localstorage.js
@@ -1,3 +1,14 @@
+/**
+ * logerr
+ *
+ * @category   scrum-board
+ * @author     Vaibhav Mehta <vaibhav@decodingweb.com>
+ * @copyright  Copyright (c) 2016 Vaibhav Mehta <https://github.com/i-break-codes>
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0 Beta
+ */
+
+
 var LocalStorage = function() {
   function set(key, val) {
     localStorage.setItem(key, val);
